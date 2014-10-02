e6a90534a7d3374598890f1068d5fe20c93baeb6
Zhizhe Zhang <zhan1442@purdue.com>
2014-10-02 13:54:40 -0400

    improve ehco to deal with the last white space

--- PA04/echo-lite.c
+++ PA04/echo-lite.c
@@ -6,7 +6,12 @@ int main(int argc, char * * argv)
 {
 	int ind;
 	for(ind = 1;ind < argc; ind++){ 
+		if(ind < argc - 1){
 		printf("%s ",argv[ind]);
+		}
+		else{
+			printf("%s",argv[ind]);
+		}
 	}
 	printf("\n");
 	return EXIT_SUCCESS;
